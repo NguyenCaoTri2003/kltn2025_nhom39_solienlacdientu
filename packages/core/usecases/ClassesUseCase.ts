5ff9e93e81ce42898e88de65e504474415190c66
Nhu <quynhnhu18121812@gmail.com>
2025-09-26 21:41:06 +0700

    update api with token

--- packages/core/usecases/ClassesUseCase.ts
+++ packages/core/usecases/ClassesUseCase.ts
@@ -1,6 +1,11 @@
 import { ClassesRepository } from "../../data/repositories/ClassesRepository";
 
-export async function getClassesByMajor(majorId: number) {
+export async function getClassesByMajor(majorId: number, user: any) {
+  const allowedRoles = ["admin", "lecturer", "student"];
+  if (!user || !allowedRoles.includes(user.role)) {
+    throw new Error("You do not have access!");
+  }
+
   const repo = new ClassesRepository();
   return await repo.getClassesByMajor(majorId);
 }
