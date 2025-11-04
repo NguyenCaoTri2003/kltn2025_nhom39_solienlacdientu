6031e4d769534c9360f0087995f1a4dad362a118
LAPTOP-LVT6JQM0\nguye <caotri2000mwg@gmail.com>
2025-11-04 19:02:06 +0700

    fix: update next.config.ts v2

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,31 +1,14 @@
-// import { NextConfig } from 'next';
-// import path from 'path';
-
-// const nextConfig: NextConfig = {
-//   webpack: (config) => {
-//     config.resolve.alias['@'] = path.resolve(__dirname, 'packages');
-//     return config;
-//   },
-//   eslint: {
-//     ignoreDuringBuilds: true
-//   }
-// };
-
-// export default nextConfig;
-import path from 'path';
 import { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    // alias @ -> packages (hoặc @packages)
-    config.resolve.alias['@packages'] = path.resolve(__dirname, '../../packages');
+    config.resolve.alias['@packages'] = path.resolve(__dirname, 'packages');
     return config;
   },
   eslint: {
     ignoreDuringBuilds: true
-  },
-  // rất quan trọng với monorepo pnpm
-  transpilePackages: ['@packages/core'], // tất cả packages bạn import trong web
+  }
 };
 
 export default nextConfig;
