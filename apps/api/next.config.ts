c4be6c9f597a38369981cc009c5dd4838cfe874d
LAPTOP-LVT6JQM0\nguye <caotri2000mwg@gmail.com>
2025-11-04 19:16:48 +0700

    fix: add file vercel.json

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -13,21 +13,25 @@
 
 // export default nextConfig;
 import { fileURLToPath } from "url";
-import path from 'path';
-import { NextConfig } from "next";
+import path from "path";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   webpack: (config) => {
+    // 👇 alias đến thư mục packages trong monorepo
     config.resolve.alias["@packages"] = path.resolve(__dirname, "../../packages");
     return config;
   },
+  // 👇 phần cực quan trọng cho monorepo
+  outputFileTracingRoot: path.join(__dirname, "../../"),
   experimental: {
     esmExternals: "loose",
+    serverComponentsExternalPackages: ["@packages", "@packages/core", "@packages/utils", "@packages/data"],
   },
 };
 
