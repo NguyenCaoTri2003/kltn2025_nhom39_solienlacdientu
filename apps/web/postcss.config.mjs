bc6e917ca5fcb1e785057e420ddb5f070ad663f4
LAPTOP-LVT6JQM0\nguye <caotri2000mwg@gmail.com>
2025-12-10 14:50:39 +0700

    update: postcss v2

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,8 +1,18 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//   },
+// }
+
+// export default config
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
-}
+};
 
-export default config
+export default config;
